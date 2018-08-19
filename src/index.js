612dd9f2bbfa2ef1bff28fa12f2048a362e84815
Aditya Srivastava <aditya.srivastava@berkeley.edu>
2018-08-18 22:04:43 -0700

    build migrations

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import 'materialize-css/dist/css/materialize.min.css';
-import './styles/index.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
